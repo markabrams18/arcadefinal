eecff7047894845bc4a62df29486292a914bf846
Jordan <jordaanyan@gmail.com>
2023-01-19 10:40:12 -0600

    a

--- arcade/src/arcade/TetrisPanel.java
+++ arcade/src/arcade/TetrisPanel.java
@@ -16,20 +16,20 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class TetrisPanel extends JPanel{
-	
-	final int WIDTH = 400;
-	final int HEIGHT = 400;
-	
+public class TetrisPanel extends JPanel {
+
+	final int WIDTH = 800;
+	final int HEIGHT = 800;
+
 	Image bgImage, rSquare, gSquare, pSquare, bSquare, randomSquare;
 
-	Timer timer, timer2; 
-	
+	Timer timer, timer2;
+
 	boolean upArrowR = false;
 	boolean downArrow = false;
 	boolean leftArrow = false;
 	boolean rightArrow = false;
-	
+
 	boolean spaceBar = false;
 	boolean cSave = false;
 
@@ -37,356 +37,349 @@ public class TetrisPanel extends JPanel{
 	TBlock t = new TBlock();
 	ZShape z = new ZShape();
 	OBlock o = new OBlock();
-	//297
-	int x =52;
+	// 297
+	int x = 52;
 	int y = 26;
-	//29
+	// 29
 	int score = 0;
 	String stScore;
-	
+
 	private final int blockSize = 26;
-	
+
 	private final int brdWidth = 10;
 	private final int brdHeight = 24;
-	
-	
-	
+
 	int num = 0;
 	int random = 0;
-	
+
 	int[][] temp = new int[3][3];
-	
-	int tempx = 2;
+
+	int tempx = 0;
 	int tempy = 0;
-	
+
+	int curX = 2;
+	int curY = 0;
+
 	int turn = 1;
 	int[][] tempSetUp = new int[12][25];
 	int[][] currPos = new int[12][25];
 	int lineCount = 1;
-	
+
 	boolean flag = true;
 	boolean once = true;
-	
-	TetrisPanel(){
-		
+
+	TetrisPanel() {
+
 		bgImage = new ImageIcon("TetrisBG.png").getImage();
 		rSquare = new ImageIcon("redBlock.png").getImage();
 		bSquare = new ImageIcon("blueBlock.png").getImage();
 		gSquare = new ImageIcon("greenBlock.png").getImage();
 		pSquare = new ImageIcon("purpleBlock.png").getImage();
-		
-		
+
 		Random r = new Random();
 		Random colorR = new Random();
-		
-		num = r.nextInt(4)+1;
-		
+
+		num = r.nextInt(4) + 1;
+
 		System.out.println("Random Num Color Is: " + num);
 
-		
-		random = r.nextInt(4)+1;
-		System.out.println("Random Number is: "+random);
-		
+		random = r.nextInt(4) + 1;
+		System.out.println("Random Number is: " + random);
 
-		
-		this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		this.setFocusable(true);
-		
+
 		this.addKeyListener(new KeyListener() {
 
 			@Override
 			public void keyTyped(KeyEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void keyPressed(KeyEvent e) {
-				switch(e.getKeyCode()) {
-				//left arrow
+				switch (e.getKeyCode()) {
+				// left arrow
 				case 37:
-				if(x > 0) {
-					x-=26;
-					System.out.println(x);
-				}
-				break;
-				
-				//up arrow
-				case 38: turn +=1;
-					if(turn == 5) {
+					if (x > 0) {
+						x -= 26;
+						System.out.println(x);
+					}
+					break;
+
+				// up arrow
+				case 38:
+					turn += 1;
+					if (turn == 5) {
 						turn = 1;
 					}
-				break;
-				
-				//right arrow
+					break;
+
+				// right arrow
 				case 39:
-				if(x < 200) {
-					x+= 26;
-				}
-				break;
-				//down arrow
-				case 40: 				
-					if(y < 572) {
-						y+= 26;
+					if (x < 200) {
+						x += 26;
+					}
+					break;
+				// down arrow
+				case 40:
+					if (y < 572) {
+						y += 26;
 						System.out.println(y);
 					}
-				break;
-				
-				case 49: spaceBar = true;break;
-				
-				case 67: cSave = true;break;
-				
-				default: System.out.println(e.getKeyCode());
+					break;
+
+				case 49:
+					spaceBar = true;
+					break;
+
+				case 67:
+					cSave = true;
+					break;
+
+				default:
+					System.out.println(e.getKeyCode());
 				}
-				
 
 			}
 
 			@Override
 			public void keyReleased(KeyEvent e) {
-				switch(e.getKeyCode()) {
-				case 38: upArrowR = false;break;
-				case 40: downArrow = false;break;
-				
-				case 37: leftArrow = false;break;
-				case 39: rightArrow = false;break;
-				
-				case 49: spaceBar = false;break;
-				case 67: cSave = false;break;
-				
-				default: System.out.println(e.getKeyCode());
-				
+				switch (e.getKeyCode()) {
+				case 38:
+					upArrowR = false;
+					break;
+				case 40:
+					downArrow = false;
+					break;
+
+				case 37:
+					leftArrow = false;
+					break;
+				case 39:
+					rightArrow = false;
+					break;
+
+				case 49:
+					spaceBar = false;
+					break;
+				case 67:
+					cSave = false;
+					break;
+
+				default:
+					System.out.println(e.getKeyCode());
+
 				}
 			}
-			
+
 		});
-		//pictures
+		
+		// pictures
 		bgImage = new ImageIcon("blackBG.png").getImage();
 		rSquare = new ImageIcon("redBlock.png").getImage();
 		bSquare = new ImageIcon("blueBlock.png").getImage();
 		gSquare = new ImageIcon("greenBlock.png").getImage();
 		pSquare = new ImageIcon("purpleBlock.png").getImage();
-		
-		
-		
-		
+
 		timer = new Timer(50, new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(once) {
-					tempSetUp();
-					
-					
-					for(int j = 0; j < tempSetUp[0].length; j ++ ) {
-						for(int i = 0; i < tempSetUp.length; i ++) {
-							
-							
-							System.out.print(tempSetUp[i][j] + " ");
-						}
-						System.out.println(" ");
-					}
-					
-					once = false;
-				}
-				
-				
-				
-				
-				
-				
-				
 
 				
 				// gets the shape
-				
-					if(num == 1) {
-						randomSquare = rSquare;
-						
-					}else if(num == 2) {
-						randomSquare = gSquare;
-						
-					}else if (num == 3) {
-						randomSquare = bSquare;
-						
-					}else if(num == 4) {
-						randomSquare = pSquare;
-						
-					}
-					
-					if(random == 1) {
-						l.upShape();
-						temp = l.getShape();
-						
-					}else if(random == 2) {
-						t.upShape();
-						temp = t.getShape();
-						
-					}else if(random == 3) {
-						z.upShape();
-						temp = z.getShape();
-						
-					}else if(random == 4) {
-						
-						o.upShape();
-						temp = o.getShape();
-					}
-					
-					if(random == 1) {
-						
-						if(turn==1) {
-							
-							l.upShape();
-							
-						}else if(turn==2) {
-							
-							if(x < 0) {
-								x+=26;
-								
-							}
-								
-							l.rightShape();
-							
-						}else if(turn==3) {
-							
-							System.out.println(x);
-							if(x < 0) {
-								x+= 26;
-							}
-							
-							l.downShape();
-						}else if(turn ==4){
-							
-							if(x < 0) {
-								x+= 26;
-								
-							}
-							
-							l.leftShape();
-						}
-						
-						temp = l.getShape();
-						
-					}else if(random == 2) {
-						
-						if(turn==1) {
-							
-							if(x < 0) {
-								x+= 26;
-							}
-							
-							t.upShape();
-						}else if(turn==2) {
-							
-							
-							t.rightShape();
-						}else if(turn==3) {
-							
-							if(x < 0) {
-								x+= 26;
-							}
 
-							t.downShape();
-						}else if(turn ==4){
-							
-							if(x < 0) {
-								x+= 26;
-							}
-							
-							t.leftShape();
+				if (num == 1) {
+					randomSquare = rSquare;
+
+				} else if (num == 2) {
+					randomSquare = gSquare;
+
+				} else if (num == 3) {
+					randomSquare = bSquare;
+
+				} else if (num == 4) {
+					randomSquare = pSquare;
+
+				}
+
+				if (random == 1) {
+					l.upShape();
+					temp = l.getShape();
+
+				} else if (random == 2) {
+					t.upShape();
+					temp = t.getShape();
+
+				} else if (random == 3) {
+					z.upShape();
+					temp = z.getShape();
+
+				} else if (random == 4) {
+
+					o.upShape();
+					temp = o.getShape();
+				}
+
+				if (random == 1) {
+
+					if (turn == 1) {
+
+						l.upShape();
+
+					} else if (turn == 2) {
+
+						if (x < 0) {
+							x += 26;
+
 						}
-						
-						temp = t.getShape();
-						
-					}else if(random == 3) {
-						
-						if(turn==1) {
-							
-							if(x < 0) {
-								x+= 26;
-							}
-							
-							z.upShape();
-						}else if(turn==2) {
-							
-							
-							z.rightShape();
-						}else if(turn==3) {
-							
-							if(x < 0) {
-								x+= 26;
-							}
-							
-							z.downShape();
-						}else if(turn ==4){
-							
-							if(x < 0) {
-								x+= 26;
-							}
-							
-							z.leftShape();
+
+						l.rightShape();
+
+					} else if (turn == 3) {
+
+						System.out.println(x);
+						if (x < 0) {
+							x += 26;
 						}
-						
-						temp = z.getShape();
-						
-					}else if(random == 4) {
-						if(x < 0) {
-							x+= 26;
+
+						l.downShape();
+					} else if (turn == 4) {
+
+						if (x < 0) {
+							x += 26;
+
 						}
-						
-						temp = o.getShape();
+
+						l.leftShape();
 					}
-					
-				
-				
-				
-				
-					
-					
-				
-				//stScore = Integer.toString(score);
+
+					temp = l.getShape();
+
+				} else if (random == 2) {
+
+					if (turn == 1) {
+
+						if (x < 0) {
+							x += 26;
+						}
+
+						t.upShape();
+					} else if (turn == 2) {
+
+						t.rightShape();
+					} else if (turn == 3) {
+
+						if (x < 0) {
+							x += 26;
+						}
+
+						t.downShape();
+					} else if (turn == 4) {
+
+						if (x < 0) {
+							x += 26;
+						}
+
+						t.leftShape();
+					}
+
+					temp = t.getShape();
+
+				} else if (random == 3) {
+
+					if (turn == 1) {
+
+						if (x < 0) {
+							x += 26;
+						}
+
+						z.upShape();
+					} else if (turn == 2) {
+
+						z.rightShape();
+					} else if (turn == 3) {
+
+						if (x < 0) {
+							x += 26;
+						}
+
+						z.downShape();
+					} else if (turn == 4) {
+
+						if (x < 0) {
+							x += 26;
+						}
+
+						z.leftShape();
+					}
+
+					temp = z.getShape();
+
+				} else if (random == 4) {
+					if (x < 0) {
+						x += 26;
+					}
+
+					temp = o.getShape();
+				}
+				setUpPrint();
+
+				// stScore = Integer.toString(score);
 				repaint();
 			}
-			
+
 		});
 		timer.start();
-		
-		timer2 = new Timer (1000, new ActionListener() {
+
+		timer2 = new Timer(1000, new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				
-				if(y <= 600) {
-					y+= 27;
+
+				if (y <= 600) {
+					y += 27;
 				}
-				
+
 			}
-			
+
 		});
-		
-		//timer2.start();
+
+		// timer2.start();
 	}
-	
+
+	public void setUpPrint() {
+		for (int j = 0; j < tempSetUp[0].length; j++) {
+			for (int i = 0; i < tempSetUp.length; i++) {
+
+				System.out.println(tempSetUp[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
 	public void tempSetUp() {
-		for(int j = 0; j < tempSetUp[0].length; j ++ ) {
-			for(int i = 0; i < tempSetUp.length; i ++) {
-				
+		for (int j = 0; j < tempSetUp[0].length; j++) {
+			for (int i = 0; i < tempSetUp.length; i++) {
+
 				tempSetUp[i][j] = 0;
-				
+
 			}
 		}
 	}
-	
+
 	public void FinalSetUp() {
-		for(int j = 0; j < currPos[0].length; j ++ ) {
-			for(int i = 0; i < currPos.length; i ++) {
-				
+		for (int j = 0; j < currPos[0].length; j++) {
+			for (int i = 0; i < currPos.length; i++) {
+
 				currPos[i][j] = 0;
-				
-				if(i == 0 || i == 9) {
+
+				if (i == 0 || i == 11) {
 					currPos[i][j] = 1;
-				}else if(j == 23) {
+				} else if (j == 24) {
 					currPos[i][j] = 1;
 				}
 				System.out.print(currPos[i][j] + " ");
@@ -394,91 +387,101 @@ public class TetrisPanel extends JPanel{
 			System.out.println(" ");
 		}
 
-		
-		
-		
 	}
-	
+
 	public void paint(Graphics g) {
-		Graphics2D g2D = (Graphics2D)g;
-		
+		Graphics2D g2D = (Graphics2D) g;
+		tempSetUp();
+
 		g2D.drawImage(bgImage, 0, 0, null);
-		
 
 		g2D.setColor(Color.white);
-		
-		for(int i = 0; i <= brdHeight; i ++) {
-			
-			g2D.drawLine(0, i*blockSize, brdWidth*blockSize, i * blockSize);
+
+		for (int i = 0; i <= brdHeight; i++) {
+
+			g2D.drawLine(0, i * blockSize, brdWidth * blockSize, i * blockSize);
 		}
-		
-		for(int i = 0; i <= brdWidth; i ++) {
-			
-			g2D.drawLine(i * blockSize, 0, i * blockSize, brdHeight*26);
+
+		for (int i = 0; i <= brdWidth; i++) {
+
+			g2D.drawLine(i * blockSize, 0, i * blockSize, brdHeight * 26);
 		}
-		
-		
+
 		/**
-		Font font = new Font("Serif", Font.PLAIN, 40);		
-		g2D.setFont(font);
-		g2D.setColor(Color.white);
-		g2D.drawString(stScore, 50,100);
-		*/
-		
-		if(temp[0][0] == 1) {
-			tempx=x-26;
-			tempy=y-26;
+		 * Font font = new Font("Serif", Font.PLAIN, 40); g2D.setFont(font);
+		 * g2D.setColor(Color.white); g2D.drawString(stScore, 50,100);
+		 */
+
+		if (temp[0][0] == 1) {
+			tempx = x - 26;
+			tempy = y - 26;
 			g2D.drawImage(randomSquare, tempx, tempy, null);
-			
+
+			tempSetUp[curX][curY] = 1;
+
 		}
-		if(temp[1][0] == 1) {
-			tempx=x;
-			tempy=y-26;
+		if (temp[1][0] == 1) {
+			tempx = x;
+			tempy = y - 26;
 			g2D.drawImage(randomSquare, tempx, tempy, null);
-			
+
+			tempSetUp[curX + 1][curY] = 1;
 		}
-		if(temp[2][0] == 1) {
-			tempx=x+26;
-			tempy=y-26;
+		if (temp[2][0] == 1) {
+			tempx = x + 26;
+			tempy = y - 26;
 			g2D.drawImage(randomSquare, tempx, tempy, null);
-			
+
+			tempSetUp[curX + 2][curY] = 1;
 		}
-		if(temp[0][1] == 1) {
-			tempx = x-26;
+		if (temp[0][1] == 1) {
+			tempx = x - 26;
 			tempy = y;
 			g2D.drawImage(randomSquare, tempx, tempy, null);
-			
+
+			tempSetUp[curX][curY + 1] = 1;
+
 		}
-		if(temp[1][1] == 1) {
-			tempx=x;
-			tempy=y;
-			g2D.drawImage(randomSquare, tempx, tempy, null);
-			
-		}
-		if(temp[2][1] == 1) {
-			tempx=x+26;
-			tempy=y;
-			g2D.drawImage(randomSquare, tempx, tempy, null);
-			
-		}
-		if(temp[0][2] == 1) {
-			tempx = x-26;
-			tempy = y+26;
-			g2D.drawImage(randomSquare, tempx, tempy, null);
-			
-		}
-		if(temp[1][2] == 1) {
+		if (temp[1][1] == 1) {
 			tempx = x;
-			tempy = y+26;
+			tempy = y;
 			g2D.drawImage(randomSquare, tempx, tempy, null);
-			
+
+			tempSetUp[curX + 1][curY + 1] = 1;
+
 		}
-		if(temp[2][2] == 1) {
-			tempx=x+26;
-			tempy=y+26;
+		if (temp[2][1] == 1) {
+			tempx = x + 26;
+			tempy = y;
 			g2D.drawImage(randomSquare, tempx, tempy, null);
-			
+
+			tempSetUp[curX + 2][curY + 1] = 1;
+
 		}
-		
+		if (temp[0][2] == 1) {
+			tempx = x - 26;
+			tempy = y + 26;
+			g2D.drawImage(randomSquare, tempx, tempy, null);
+
+			tempSetUp[curX][curY + 2] = 1;
+
+		}
+		if (temp[1][2] == 1) {
+			tempx = x;
+			tempy = y + 26;
+			g2D.drawImage(randomSquare, tempx, tempy, null);
+
+			tempSetUp[curX + 1][curY + 2] = 1;
+
+		}
+		if (temp[2][2] == 1) {
+			tempx = x + 26;
+			tempy = y + 26;
+			g2D.drawImage(randomSquare, tempx, tempy, null);
+
+			tempSetUp[curX + 2][curY + 2] = 1;
+
+		}
+
 	}
 }
