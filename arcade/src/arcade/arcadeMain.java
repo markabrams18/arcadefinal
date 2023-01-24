bcd39d202156c007140351f274aaea1a0e3afd8e
Jordan <jordaanyan@gmail.com>
2023-01-24 10:35:58 -0600

    ads

--- arcade/src/arcade/arcadeMain.java
+++ arcade/src/arcade/arcadeMain.java
@@ -6,7 +6,7 @@ public class arcadeMain {
 
 		arcadeTitlePage a = new arcadeTitlePage();
 		a.getGame();
-
+//i hate tetris
 		AnimationTetris p = new AnimationTetris();
 		//brickbreaker b = new brickbreaker();
 		//DUMB STUFF
